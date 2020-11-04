ae51cf71cbbecb8aa9e352ed249f06453d8a24ba
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-04 18:57:33 +0000

    testing even function

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+// can directly put the functions in the console
+
+console.log(isEven(10));
+console.log(isEven(11));
+
+
