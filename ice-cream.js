9ec9ed13e4c0c567ae0bf52c36078406497bb122
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-04 22:32:45 +0000

    Arrays - Ice Cream! Assignment

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//Add a Flavour
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//Get the First Flavour
+console.log(iceCreamFlavours[0]);
+
+//Get the Last Flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//Total Number of Flavours
+console.log(iceCreamFlavours.length);
+
