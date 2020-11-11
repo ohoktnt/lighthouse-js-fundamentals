ea553ed0baec34f9b1af0da93e724bdd12daa45f
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-11 17:18:58 +0000

    Merge Arrays Assignment Stretch

---
+++ mergeArrays.js
@@ -0,0 +1,12 @@
+// Merge Arrays Assignment Stretch
+// function to merge two arrays and keep the data sorted
+
+function merge (array1, array2){
+  let newArray = array1.concat(array2);
+  newArray.sort();
+  return newArray;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
