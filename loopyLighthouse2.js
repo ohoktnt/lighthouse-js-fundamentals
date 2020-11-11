e8282e4194fc34d7eedf3bc80b6ba96c82bbf4b7
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-11 17:11:32 +0000

    Loopy Lighthouse 2 Assignment Stretch

---
+++ loopyLighthouse2.js
@@ -0,0 +1,19 @@
+// Looopy Lighthouse 2 Assignment
+// print the numbers in a range, replacing multiples of the second argument
+// with the words from the third argument
+
+function loopyLighthouse (range, multiples, words) {
+  for (let num = range[0]; num <= range[1]; num++){
+    if (num % multiples[0] === 0 && num % multiples[1] === 0){
+      console.log(words[0] + words[1]);
+    } else if (num % multiples[0] === 0){
+      console.log(words[0]);
+    } else if (num % multiples[1] === 0){
+      console.log(words[1]);
+    } else {
+      console.log(num);
+    }
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
